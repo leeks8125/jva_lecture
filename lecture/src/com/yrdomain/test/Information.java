69fba27d69099917fd51e88cb21b16786ea6891b
km <km@DESKTOP-6JSJAMP>
2021-01-11 18:48:27 +0900

    first

---
+++ lecture/src/com/yrdomain/test/Information.java
@@ -0,0 +1,14 @@
+package com.yrdomain.test;
+
+public class Information {
+	String subject;
+	String writer;
+	String date;
+	String view;
+	String like;
+	
+	
+	public Information(String subject,String writer,String date,String view,String like ) {
+		this.subject = subject;
+	}
+}
