5bb85f3cbf4b0242d27595670c05045908c26181
km <km@DESKTOP-6JSJAMP>
2021-01-12 11:21:35 +0900

    first

---
+++ lecture/src/com/yrdomain/ch07/DmbCellPhone.java
@@ -0,0 +1,23 @@
+package com.yrdomain.ch07;
+
+public class DmbCellPhone extends CellPhone{
+	int channel;
+	
+	DmbCellPhone(String model, String color, int channel){
+		this.model = model;
+		this.color = color;
+		this.channel = channel;
+	}
+	
+	void turnOnDmb() {
+		System.out.println("채널" + channel + "번DMB방송수신을 시작합니다");
+	}
+	void changeChannelDmb(int channel) {
+		this.channel = channel;
+		System.out.println("채널"+channel+"번으로 바꿉니다");
+	}
+	void turnOffDmb() {
+		System.out.println("DMB방송수신을 멈추니다");
+	}
+	
+}
