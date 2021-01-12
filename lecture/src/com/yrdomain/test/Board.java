faa997396d7a292685dadc252ecfcceeece48de9
km <km@DESKTOP-6JSJAMP>
2021-01-12 17:14:50 +0900

    first

---
+++ lecture/src/com/yrdomain/test/Board.java
@@ -0,0 +1,44 @@
+package com.yrdomain.test;
+
+public class Board {
+	String name;
+	int like;
+	String share;
+	String comment;
+	String more;
+	
+	public Board(String name,int like,String share,String comment,String more) {
+		this.name = name;
+		this.like = like;
+		this.share = share;
+		this.comment = comment;
+		this.more = more;	
+	}
+	
+	//올린사람 게시물
+	public void ckname() {
+		System.out.println();
+	}
+	//좋아요누름
+	public void cklike() {
+		System.out.println();
+	}
+	//댓글보기
+	public void ckcomment() {
+		System.out.println();
+	}
+	//댓글쓰기
+	public void writcomment() {
+		System.out.println();
+	}
+	//공유하기
+	public void ckshare() {
+		System.out.println();
+	}
+	//더보기
+	public void ckmore() {
+		System.out.println();
+	}
+}
+
+
