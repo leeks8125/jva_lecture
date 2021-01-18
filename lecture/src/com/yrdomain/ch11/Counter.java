64d6fbba3a990f9f544b46f41a12b11283351876
km <km@DESKTOP-6JSJAMP>
2021-01-18 17:39:09 +0900

    first

---
+++ lecture/src/com/yrdomain/ch11/Counter.java
@@ -0,0 +1,14 @@
+package com.yrdomain.ch11;
+
+public class Counter {
+	private int no;
+	
+	public Counter(int no) {
+		this.no=no;
+		
+	}
+	@Override
+	protected void finalize() throws Throwable {
+		System.out.println(no+"¹ø °´Ã¼ÀÇ finalizw()°¡ ½ÇÇàµÊ");
+	}
+}
---
+++ lecture/src/com/yrdomain/ch11/Member1.java
@@ -0,0 +1,27 @@
+package com.yrdomain.ch11;
+
+public class Member1 implements Cloneable {
+	public String id;
+	public String name;
+	public String password;
+	public int age;
+	public boolean adult;
+	
+	public Member1(String id, String name, String password, int age, boolean adult) {
+		this.id = id;
+		this.name = name;
+		this.password = password;
+		this.age = age;
+		this.adult = adult;
+	}
+	
+	public Member1 getMember1() {
+	Member1 cloned= null;
+	try {
+		cloned =(Member1) clone();
+	} catch (CloneNotSupportedException e) {}
+	
+	return cloned;
+	}
+
+}
